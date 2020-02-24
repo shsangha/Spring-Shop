59dfe8a1596764d5b08816a3e25c9bc10ad06d25
shsangha <sssangha@ucalgary.ca>
2020-02-24 01:45:42 -0700

    configuration bean for bcrypt to use the password encoder to avoid saving plain text passwords

---
+++ src/main/java/shsangha/springstore/demo/config/BcryptConfig.java
@@ -0,0 +1,16 @@
+package shsangha.springstore.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class BcryptConfig {
+
+    @Bean
+    public BCryptPasswordEncoder initBean(){
+
+        return new BCryptPasswordEncoder();
+    }
+
+}
